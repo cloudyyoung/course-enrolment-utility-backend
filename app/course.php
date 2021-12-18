af6a420be2898fb50f13c9409f09725570b6868a
lybned <60757167+lybned@users.noreply.github.com>
2021-12-17 20:07:16 -0700

    Update course.php

--- app/course.php
+++ app/course.php
@@ -20,7 +20,7 @@ class course
 
         $sql = "SELECT `C`.`course_id`, `C`.`no_gpa`, `C`.`repeat`, `C`.`code`, `C`.`number`, `C`.`units`, `C`.`topic`, `C`.`notes`, `C`.`description`, `C`.`credit`, `H`.`hours`, `A`.`aka`, `T`.`time_length`
                 FROM `course` as `C`, `hours` as `H`, `AKA` as `A`, `time_length` as `T`
-                WHERE `C`.`course_id` = `H`.`course_id AND
+                WHERE `C`.`course_id` = `H`.`course_id` AND
                 `C`.`course_id` = `A`.`course_id` AND
                 `C`.`course_id` = `T`.`course_id` AND
                 `C`.`code` = '$code' AND `C`.`number` = '$number'";
