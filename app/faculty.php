0ac2b10982845912104cb65bb84cf270dcd28aa9
Cloudy Young <cloudy.young@outlook.com>
2021-12-21 17:26:15 -0700

    Update faculty.php

--- app/faculty.php
+++ app/faculty.php
@@ -4,65 +4,8 @@ namespace App;
 
 use Flight;
 
-//aka --> contractable
-//AKA --> course_Id
-/*
- *DONT FORGET TO CHANGE THE TABLE NAME AND ATTRIBUTES TO MATCH THE MYSQL DATABASE
- */
-
 class Faculty extends Contactable
 {
-    private static function process_info_contactable($result, $result1, $result2, $result3, $result4, $result5)
-    {
-        //if one of them is incorrect then return false
-        if (!$result || !$result1 || !$result2 || !$result3 || !$result4 || !$result5) {
-            return false;
-        }
-
-        //transform the result into readable formats
-        $result = $result->fetch_all(MYSQLI_ASSOC);
-        $result = $result[0];
-        $result1 = $result1->fetch_all(MYSQLI_ASSOC);
-        $result2 = $result2->fetch_all(MYSQLI_ASSOC);
-        $result3 = $result3->fetch_all(MYSQLI_ASSOC);
-        $result4 = $result4->fetch_all(MYSQLI_ASSOC);
-        $result5 = $result5->fetch_all(MYSQLI_ASSOC);
-
-        //transform the result from other results into result
-        $result["aka"] = [];
-        $result["phone"] = [];
-        $result["website"] = [];
-        $result["room"] = [];
-        $result["email"] = [];
-
-
-        foreach ($result1 as &$insert) {
-            array_push($result["aka"], $insert["aka"]);
-        }
-
-
-        foreach ($result2 as &$insert) {
-            array_push($result["phone"], $insert["phone"]);
-        }
-
-
-        foreach ($result3 as &$insert) {
-            array_push($result["website"], $insert["website"]);
-        }
-
-
-        foreach ($result4 as &$insert) {
-            array_push($result["room"], $insert["room"]);
-        }
-
-        foreach ($result5 as &$insert) {
-            array_push($result["email"], $insert["email"]);
-        }
-
-        return $result;
-    }
-
-
     // End point 7.1 - All faculty
     public static function AllFaculty()
     {
